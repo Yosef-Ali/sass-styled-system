5564bf4f0aee7617d8f1f06bef1714c632360732
apple <apple@apples-MacBook-Pro.local>
2021-09-17 15:02:30 +0300

    adding purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require('gulp');
+const purgecss = require('gulp-purgecss');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-	return src('ethiopia/**/*.scss').pipe(sass()).pipe(dest('css'));
+	return src('ethiopia/**/*.scss')
+		.pipe(sass())
+		.pipe(purgecss({ content: ['*.html'] }))
+		.pipe(dest('css'));
 }
 
 function watchTask() {
