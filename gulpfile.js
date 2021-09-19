3388e19ea4a60e88f194702b6979fdfc26bac21a
apple <apple@apples-MacBook-Pro.local>
2021-09-19 19:37:08 +0300

    some modification

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
 const { src, dest, watch, series } = require('gulp');
-const purgecss = require('gulp-purgecss');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-	return src('ethiopia/**/*.scss')
-		.pipe(sass())
-		.pipe(purgecss({ content: ['*.html'] }))
-		.pipe(dest('css'));
+	return src('shinobi/**/*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function watchTask() {
-	watch(['ethiopia/**/*.scss'], buildStyles);
+	watch(['shinobi/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
